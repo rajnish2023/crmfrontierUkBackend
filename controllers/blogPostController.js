899a963e5b7ac8e38d5b5d6d58025f8cc11a0f76
rajnish2023 <rajnish.kumar@dynamicssquare.com>
2026-06-22 14:58:22 +0530

    //uploads to /uploads

--- controllers/blogPostController.js
+++ controllers/blogPostController.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, '//uploads'); 
+    cb(null, '/uploads'); 
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));  
